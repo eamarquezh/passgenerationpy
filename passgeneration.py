8ff28ddce771f5d9e63d8a55f8e4797e9b4b2b97
ericmanweb <eamarquezh@gmail.com>
2019-05-10 21:43:36 -0500

    inicio2

--- passgeneration.py
+++ passgeneration.py
@@ -1,3 +1,8 @@
+import random
 #este programa te ayudara a generar una posible clave segura
+letras=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 passw="default"
+passw=""
+for i in range(0,7):
+    passw=passw+letras[random.randint(0, 25)]
 print("La clave propuesta es",passw)
